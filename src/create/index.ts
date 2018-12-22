80dba5b94420b6e76ef0e9f477114947a3ed0342
Ryan Naddy <rnaddy@corp.acesse.com>
2018-12-22 09:24:06 -0600

    Made changes

--- src/create/index.ts
+++ src/create/index.ts
@@ -9,41 +9,105 @@ export interface CreateOptions {
   type: 'typescript' | 'javascript'
 }
 
-export function makeNewProject(createOptions: CreateOptions) {
+const sourceLocation = 'https://github.com/red5-server/red5'
+
+export async function makeNewProject(createOptions: CreateOptions) {
   let projectDir = path.join(process.cwd(), createOptions.project)
-  fs.stat(projectDir, (err, stats) => {
-    if (err) {
-      // Could not find the directory, lets create the project
-      return cloneRepository(projectDir, createOptions)
+  console.log(info(`Attempting to create project "${createOptions.project}"`))
+  let built = await build(projectDir, createOptions)
+  if (built) {
+    console.log(info(`Attempting to install "node_modules"`))
+    let installed = await installModules(projectDir)
+    if (installed) {
+      console.log(info(`Starting the test server`))
+      await startTestServer(projectDir)
+      return true
     }
-    if (process.platform == 'win32') {
-      // Windows does not allow for files and directories to be named the same
-      if (stats.isDirectory() || stats.isFile()) {
-        // The stats of the path is a directory, we can't overwrite it
-        return console.log(error(`The project "${createOptions.project}" already exists, delete the directory and run the new command again or use a different project name.`))
-      } else {
-        return cloneRepository(projectDir, createOptions)
+  }
+  return false
+}
+
+async function build(projectDir: string, createOptions: CreateOptions) {
+  return new Promise<boolean>(resolve => {
+    fs.stat(projectDir, async (err, stats) => {
+      if (err) {
+        // Could not find the directory, lets create the project
+        return resolve(await cloneRepository(projectDir, createOptions))
       }
-    } else {
-      if (stats.isDirectory()) {
-        // The stats of the path is a directory, we can't overwrite it
-        return console.log(error(`The project "${createOptions.project}" already exists, delete the directory and run the new command again or use a different project name.`))
+      if (process.platform == 'win32') {
+        // Windows does not allow for files and directories to be named the same
+        if (stats.isDirectory() || stats.isFile()) {
+          // The stats of the path is a directory, we can't overwrite it
+          return console.log(error(`The project "${createOptions.project}" already exists, delete the directory and run the new command again or use a different project name.`))
+        } else {
+          return resolve(await cloneRepository(projectDir, createOptions))
+        }
       } else {
-        // The stats of the path is a file, we can make a new project
-        return cloneRepository(projectDir, createOptions)
+        if (stats.isDirectory()) {
+          // The stats of the path is a directory, we can't overwrite it
+          return console.log(error(`The project "${createOptions.project}" already exists, delete the directory and run the new command again or use a different project name.`))
+        } else {
+          // The stats of the path is a file, we can make a new project
+          return resolve(await cloneRepository(projectDir, createOptions))
+        }
       }
-    }
+    })
   })
 }
 
-function cloneRepository(projectDir: string, createOptions: CreateOptions) {
-  clone('https://github.com/red5-server/framework', projectDir, { shallow: true }, () => {
-    if (createOptions.type == 'javascript') {
-      cp.exec(`node ./node_modules/.bin/tsc "${projectDir}"`, (err, stdout, stderr) => {
-        if (err) return console.log(error(stderr))
-        return console.log(info('JavaScript project has been built!'))
-      })
-    }
-    return console.log(info('TypeScript project has been built!'))
+async function cloneRepository(projectDir: string, createOptions: CreateOptions) {
+  return new Promise<boolean>(resolve => {
+    clone(sourceLocation, projectDir, { shallow: true }, async () => {
+      if (createOptions.type == 'javascript') {
+        return resolve(await compileTypeScript(projectDir))
+      }
+      console.log(info('TypeScript successfully built'))
+      return resolve(true)
+    })
+  })
+}
+
+async function compileTypeScript(projectDir: string) {
+  return new Promise<boolean>(resolve => {
+    cp.exec(`node ./node_modules/.bin/tsc "${projectDir}"`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(error(stderr))
+        return resolve(false)
+      }
+      console.log(info('JavaScript successfully built'))
+      return resolve(true)
+    })
+  })
+}
+
+async function installModules(projectDir: string) {
+  return new Promise<boolean>(resolve => {
+    cp.exec(`cd "${projectDir}" && npm install`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(error(stderr))
+        return resolve(false)
+      }
+      console.log(info('node_modules successfully installed'))
+      return resolve(true)
+    })
+  })
+}
+
+async function startTestServer(projectDir: string) {
+  return new Promise<boolean>(resolve => {
+    cp.fork(`${path.join(projectDir, 'index.js')}`)
+
+    var url = 'http://localhost:5000'
+    var start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open')
+    cp.exec(start + ' ' + url)
+
+    // cp.exec(`cd "${projectDir}" && node index.js`, (err, stdout, stderr) => {
+    //   if (err) {
+    //     console.log(error(stderr))
+    //     return resolve(false)
+    //   }
+    //   console.log(info('node_modules successfully installed'))
+    //   return resolve(true)
+    // })
   })
 }
