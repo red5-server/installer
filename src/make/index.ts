592dda6cc4b26cf0d9807302bc3b8f17d0db308a
Ryan Naddy <rnaddy@corp.acesse.com>
2019-05-10 16:38:49 -0500

    bugs

--- src/make/index.ts
+++ src/make/index.ts
@@ -1,6 +1,3 @@
-import { isRed5Project } from '../helper'
-import { error } from '..'
-import * as mkdirp from 'mkdirp'
 import * as path from 'path'
 import * as fs from 'fs'
 
@@ -14,86 +11,34 @@ export interface MakeMiddlewareOptions {
   name: string
 }
 
-const PATH = process.cwd()
-const RESOURCES = path.join(__dirname, '../../resources')
+/** @type {string} The Location of the current directory the script is executing within (This is where a red5 project should be living) */
+export const PATH: string = process.cwd()
+
+/** @type {string} The root location of where the resources such as template files live */
+export const RESOURCES: string = path.join(__dirname, '../../resources')
 
 /**
- * Creates a controller in the "app/controllers" directory
+ * Tests if a path is the location of an existing file or directory
+ *
+ * @export
+ * @param {string} path The path to the file
+ * @returns {Promise<boolean>}
  */
-export async function makeController(options: MakeControllerOptions) {
-  if (!(await isRed5Project())) return console.log(error('This is not a red5 project'))
-
-  if (options.api && options.resource) {
-    return console.log(error('Either use "api" or "resource" but do not use both'))
-  }
-
-  // Get the path to the controller
-  const CONTROLLER_PATH = options.name.split('/').slice(0, -1).join('/')
-
-  // Get the name of the controller
-  const CONTROLLER = options.name.split('/').pop()
-
-  // Create the directory in the controllers folder
-  mkdirp.sync(path.join(PATH, 'app/controllers', CONTROLLER_PATH))
-
-  return new Promise<void>(async resolve => {
-    let controllerPath = path.join(PATH, 'app/controllers', CONTROLLER_PATH, CONTROLLER + '.js')
-    if (await isFile(controllerPath)) {
-      console.log(error('This controller already exists.'))
-      return resolve()
-    }
-
-    // Get the proper template to use
-    let controllerToUse = 'controller'
-    if (options.api) controllerToUse = 'controller.api'
-    else if (options.resource) controllerToUse = 'controller.resource'
-
-    // Copy the template from the resources folder
-    fs.createReadStream(path.join(RESOURCES, 'templates/make/' + controllerToUse + '.tpl'))
-      .pipe(fs.createWriteStream(controllerPath))
-      .on('close', () => resolve())
-  })
-}
-
-/**
- * Creates middleware in the "app/middleware" directory
- */
-export async function makeMiddleware(options: MakeMiddlewareOptions) {
-  if (!(await isRed5Project())) return console.log(error('This is not a red5 project'))
-
-  // Get the path to the controller
-  const MIDDLEWARE_PATH = options.name.split('/').slice(0, -1).join('/')
-
-  // Get the name of the controller
-  const MIDDLEWARE = options.name.split('/').pop() || ''
-
-  // Create the directory in the middleware folder
-  mkdirp.sync(path.join(PATH, 'app/middleware', MIDDLEWARE_PATH))
-  return new Promise<void>(async resolve => {
-    let middlewarePath = path.join(PATH, 'app/middleware', MIDDLEWARE_PATH, MIDDLEWARE + '.js')
-    if (await isFile(middlewarePath)) {
-      console.log(error('This middleware already exists.'))
-      return resolve()
-    }
-    // Copy the template from the resources folder
-    fs.createReadStream(path.join(RESOURCES, 'templates/make/middleware.tpl'))
-      .pipe(fs.createWriteStream(middlewarePath))
-      .on('close', () => {
-        let data = fs.readFileSync(middlewarePath).toString()
-        let replaced = replace(data, [['name', MIDDLEWARE]])
-        fs.writeFileSync(middlewarePath, replaced)
-        resolve()
-      })
-  })
-}
-
-async function isFile(path: string) {
+export async function isFile(path: string): Promise<boolean> {
   return new Promise<boolean>(resolve => {
     fs.stat(path, (err, stats) => resolve(err ? false : stats.isFile() || stats.isDirectory()))
   })
 }
 
-function replace(data: string, replacements: string[][]) {
+/**
+ * Replaces the template variables with data
+ *
+ * @export
+ * @param {string} data The template string
+ * @param {[string, string][]} replacements The replacement data where `index 0` is the key and `index 1` is the value
+ * @returns {string} The new template with the variables replaced
+ */
+export function replaceTemplateVars(data: string, replacements: [string, string][]): string {
   replacements.forEach(item => {
     let [find, replace] = item
     data = data.replace(new RegExp('\\$\\$\\{\\{' + find + '\\}\\}', 'g'), replace)
