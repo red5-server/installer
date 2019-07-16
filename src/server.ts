bc15d9de615596d3f7fce8cd2ed032698ade4a5f
Ryan Naddy <untuned20@gmail.com>
2019-07-16 08:41:28 -0500

    updated server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import * as cp from 'child_process'
 import * as path from 'path'
-// import * as fs from 'fs'
+import * as fs from 'fs'
+import * as glob from 'glob'
 import chalk from 'chalk'
+import * as sass from 'node-sass'
 import * as chokidar from 'chokidar'
 import ServerListCommand from './red5-commands/server/list';
+import mkdirp = require('mkdirp');
 
 // The main server process
 let server: null | cp.ChildProcess
@@ -20,7 +23,9 @@ chokidar.watch([
   path.join(cwd, 'app'),
   path.join(cwd, 'config'),
   path.join(cwd, 'routes')
-]).on('all', change)
+]).on('all', serverChange)
+
+chokidar.watch(path.join(cwd, 'resources/assets/styles')).on('all', sassChange)
 
 let maxRestarts = 5
 let restarts = 0
@@ -43,10 +48,15 @@ async function createServer() {
     restarts = 0
     ServerListCommand.addServer(cwd, process.pid)
   } catch (e) {
-    if (restarts++ == maxRestarts) {
-      process.kill(process.pid)
+    if (restarts++ >= maxRestarts) {
+      exit()
     }
   }
+}
+
+function exit() {
+  interval && clearInterval(interval)
+  process.kill(process.pid)
 }
 
 // Watch the server to make sure it is running
@@ -54,9 +64,45 @@ async function watch() {
   if (!server) await createServer()
 }
 
-function change() {
+function serverChange() {
   if (server) {
     console.log(chalk.blueBright(`File changed at [${new Date().toLocaleString()}] restarting the development server`))
     server.kill()
   }
+}
+
+function sassChange() {
+  glob(path.join(cwd, 'resources/assets/styles/**/*.{scss,sass,css}'), async (err, files) => {
+    for (let file of files) {
+      let f = path.parse(file)
+
+      // If this is a sass partial file skip it
+      if (f.name.startsWith('_') && (f.name.endsWith('scss') || f.name.endsWith('sass'))) continue
+
+      // Create the save file based on the source file
+      let savePath = file
+        .replace(path.join(cwd, 'resources/assets/styles'), '')
+        .replace(/(sass|scss)$/, 'css')
+
+      // make the directory so the file can be saved
+      mkdirp.sync(path.join(cwd, 'public/css', path.parse(savePath).dir))
+
+      // If the file is a css file, just copy it
+      if (f.ext == '.css') {
+        fs.createReadStream(file).pipe(fs.createWriteStream(path.join(cwd, 'public/css', savePath)))
+        continue
+      }
+
+      // This must be a main sass file, compile it
+      sass.render({
+        file,
+        outputStyle: 'compressed'
+      }, async (err, result) => {
+        if (err) return
+        let css = result.css.toString()
+        await new Promise(r => fs.writeFile(path.join(cwd, 'public/css', savePath), css, (err) => r()))
+      })
+    }
+  })
+
 }
